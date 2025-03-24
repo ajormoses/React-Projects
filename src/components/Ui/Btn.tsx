d84a07f43d370528164935008121cc45059018f8
ajormoses <ajormoses@gmail.com>
2025-03-24 22:12:02 +0100

    Refactor Btn component to add onClick prop

--- src/components/Ui/Btn.tsx
+++ src/components/Ui/Btn.tsx
@@ -3,12 +3,14 @@ import React from "react";
 interface BtnProps {
   label: string;
   customClass?: string;
+  onClick?: () => void;
 }
 
-const Btn: React.FC<BtnProps> = ({ label, customClass }) => {
+const Btn: React.FC<BtnProps> = ({ label, customClass, onClick }) => {
   return (
     <>
       <button
+        onClick={onClick}
         className={`border border-white py-4 px-14 bg-primary text-white font-medium text-base rounded-md ease-in-out duration-300 hover:scale-95 hover:opacity-90 ${customClass} `}
         type="button"
       >
