bfd0c932a988a53bd477670bf5856cd5c1f5cef4
ajormoses <ajormoses@gmail.com>
2025-03-25 22:32:01 +0100

    Refactor import of Home component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router";
-import HomePage from "./pages/Home";
+import Home from "./pages/Home";
 import ProductPage from "./pages/Product";
 import "./assets/tailwind.css";
 import "@mdi/font/css/materialdesignicons.css";
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/product" element={<ProductPage />} />
       </Routes>
     </>
