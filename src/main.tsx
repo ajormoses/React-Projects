d33cea00a683e780d5fef87d5265864526d22f34
ajormoses <ajormoses@gmail.com>
2025-03-25 22:25:55 +0100

    Refactor main.tsx to use BrowserRouter and Routes for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./assets/tailwind.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+const root = document.getElementById("root");
+
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <BrowserRouter>
+      {/* <Routes>
+        <Route path="/" element={<App />} />
+      </Routes> */}
+      <App />
+    </BrowserRouter>
+  );
+} else {
+  console.error("Root element not found");
+}
