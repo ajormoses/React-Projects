a34dc54db9de299453d1ba2182b1831004ede534
ajormoses <ajormoses@gmail.com>
2025-03-25 22:25:02 +0100

    Refactor Btn component to use clsx for dynamic class names

--- src/components/Ui/Btn.tsx
+++ src/components/Ui/Btn.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import clsx from "clsx";
 
 interface BtnProps {
   label: string;
@@ -11,7 +12,10 @@ const Btn: React.FC<BtnProps> = ({ label, customClass, onClick }) => {
     <>
       <button
         onClick={onClick}
-        className={`border border-white py-4 px-14 bg-primary text-white font-medium text-base rounded-md ease-in-out duration-300 hover:scale-95 hover:opacity-90 ${customClass} `}
+        className={clsx(
+          `border border-white py-4 px-14 bg-primary text-white font-medium text-base rounded-md ease-in-out duration-300 hover:scale-95 hover:opacity-90 `,
+          customClass
+        )}
         type="button"
       >
         {label}
