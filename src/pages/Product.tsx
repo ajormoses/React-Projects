3f78461e30576318b611baa31d03f1c993838532
ajormoses <ajormoses@gmail.com>
2025-03-25 23:52:35 +0100

    Refactor import of Footer component in Product.tsx

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Navigation/Header";
 import ProductDetails from "../components/Resources/Products/ProductDetails";
+import Footer from "../components/Navigation/Footer";
 
 const Product = () => {
   return (
     <>
       <Header />
       <ProductDetails />
+      <Footer />
     </>
   );
 };
