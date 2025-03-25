7d69175bbf34e72bbae4d13cbc75aad7126900bb
ajormoses <ajormoses@gmail.com>
2025-03-25 23:47:45 +0100

    Refactor import of Header and ProductDetails components in Product.tsx

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,7 +1,11 @@
+import Header from "../components/Navigation/Header";
+import ProductDetails from "../components/Resources/Products/ProductDetails";
+
 const Product = () => {
   return (
     <>
-      <div>dhdhdjdj</div>
+      <Header />
+      <ProductDetails />
     </>
   );
 };
