4e99db5b3385cbee8f3e2f7a4ba71e08cfea3db1
ajormoses <ajormoses@gmail.com>
2025-03-25 22:32:06 +0100

    Refactor Home component to optimize imports and structure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,25 @@
-import Home from "../components/Resources/Home";
+import MainScreen from "../components/Resources/Home/mainScreen";
+import HomeCategory from "../components/Resources/Home/HomeCategory";
+import HomeProducts from "../components/Resources/Home/HomeProducts";
+import HomeSell from "../components/Resources/Home/HomeSell";
+import HomeAds from "../components/Resources/Home/HomeAds";
+import HomeDiscount from "../components/Resources/Home/HomeDiscount";
+import HomeSummerSales from "../components/Resources/Home/HomeSummerSales";
+import Footer from "../components/Navigation/Footer";
 
-const HomePage = () => {
+const Home = () => {
   return (
     <>
-      <Home />
+      <MainScreen />
+      <HomeProducts />
+      <HomeCategory />
+      <HomeSell />
+      <HomeAds />
+      <HomeDiscount />
+      <HomeSummerSales />
+      <Footer />
     </>
   );
 };
 
-export default HomePage;
+export default Home;
