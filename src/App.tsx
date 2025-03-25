4377ab1464c4567d8fdd0af33040051c056da8ae
ajormoses <ajormoses@gmail.com>
2025-03-25 23:58:16 +0100

    Refactor scroll behavior to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, useLocation } from "react-router";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import "./assets/tailwind.css";
 import "@mdi/font/css/materialdesignicons.css";
 
 function App() {
+  const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }, [pathname]);
+
+    return null;
+  };
+
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product" element={<Product />} />
