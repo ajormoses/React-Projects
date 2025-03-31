6a2fe41d6b036e068569ba4e2e3d856b4b33042e
ajormoses <ajormoses@gmail.com>
2025-03-31 22:02:50 +0100

    Refactor Btn component to support prepend and append icons

--- src/components/Ui/Btn.tsx
+++ src/components/Ui/Btn.tsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import clsx from "clsx";
 
 interface BtnProps {
   label: string;
   customClass?: string;
   onClick?: () => void;
+  prependIcon?: ReactNode;
+  appendIcon?: ReactNode;
 }
 
-const Btn: React.FC<BtnProps> = ({ label, customClass, onClick }) => {
+const Btn: React.FC<BtnProps> = ({
+  label,
+  customClass,
+  onClick,
+  prependIcon,
+  appendIcon,
+}) => {
   return (
     <>
       <button
         onClick={onClick}
         className={clsx(
-          `border border-white py-4 px-14 bg-primary text-white font-medium text-base rounded-md ease-in-out duration-300 hover:scale-95 hover:opacity-90 `,
+          `border flex items-center justify-center border-white py-4 px-14 bg-primary text-white font-medium text-base rounded-md ease-in-out duration-300 hover:scale-95 hover:opacity-90 `,
+          (prependIcon || appendIcon) && "gap-4",
           customClass
         )}
         type="button"
       >
+        {/* Prepend Icon */}
+        {prependIcon && prependIcon}
+        {/* label */}
         {label}
+        {/* Append Icon */}
+        {appendIcon && appendIcon}
       </button>
     </>
   );
