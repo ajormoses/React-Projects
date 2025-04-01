06503cff2622196751de65dabe786fad6b8e43f8
ajormoses <ajormoses@gmail.com>
2025-04-01 15:24:53 +0100

    Add OrderSummary component to Cart

---
+++ src/components/Resources/Cart/OrderSummary.tsx
@@ -0,0 +1,9 @@
+const OrderSummary = () => {
+  return (
+    <>
+      <div className="border border-[#EBEBEB] py-14 px-4 flex flex-col gap-4"></div>
+    </>
+  );
+};
+
+export default OrderSummary;
