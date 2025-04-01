854e4d2b58e1afa4b28262482b0b0d0fbe8655ab
ajormoses <ajormoses@gmail.com>
2025-04-01 15:06:16 +0100

    Refactor itemCart component to use useState and useCallback hooks

--- src/components/Resources/Cart/itemCart.tsx
+++ src/components/Resources/Cart/itemCart.tsx
@@ -3,21 +3,24 @@ import { useState, useCallback } from "react";
 import useCurrencyFormatter from "../../../composables/useCurrencyFormatter";
 import Btn from "../../Ui/Btn";
 
-interface Props {
-  carts: {
-    item: string;
-    price: number;
-    title: string;
-    ref: string;
-  }[];
+interface CartItem {
+  item: string;
+  price: number;
+  title: string;
+  ref: string;
 }
 
-const itemCart: React.FC<Props> = ({ carts }) => {
-  const [count, setCount] = useState(0);
+interface Props {
+  carts: CartItem[];
+}
+
+const ItemCart: React.FC<Props> = ({ carts: initialCarts }) => {
+  const [carts, setCarts] = useState<CartItem[]>(initialCarts);
+  const [count, setCount] = useState(1);
   const { formatCurrency } = useCurrencyFormatter();
 
   const RemoveCart = useCallback((index: number) => {
-    carts.splice(index, 1);
+    setCarts((prevCarts) => prevCarts.filter((_, i) => i !== index));
   }, []);
 
   return (
@@ -39,7 +42,7 @@ const itemCart: React.FC<Props> = ({ carts }) => {
               <div className="flex items-center gap-0.5">
                 <Btn
                   customClass="!w-6 !h-6 !p-4 !bg-transparent !text-black !text-xl"
-                  onClick={() => setCount(count - 1)}
+                  onClick={() => setCount(Math.max(1, count - 1))}
                   label="-"
                 />
                 <p className="text-black py-2 px-4 border-[0.5px] w-[40px] h-[32px] flex justify-center items-center border-[#D9D9D9] rounded">
@@ -55,7 +58,10 @@ const itemCart: React.FC<Props> = ({ carts }) => {
                 </p>
               </div>
 
-              <TfiClose onClick={() => RemoveCart(index)} className="text-xl" />
+              <TfiClose
+                onClick={() => RemoveCart(index)}
+                className="text-xl cursor-pointer"
+              />
             </div>
           </div>
         </div>
@@ -64,4 +70,4 @@ const itemCart: React.FC<Props> = ({ carts }) => {
   );
 };
 
-export default itemCart;
+export default ItemCart;
