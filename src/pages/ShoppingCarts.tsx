bcf1f7944a87bf6b54792254112156ab8574d696
ajormoses <ajormoses@gmail.com>
2025-04-01 14:57:20 +0100

    Add Footer component to ShoppingCarts page

--- src/pages/ShoppingCarts.tsx
+++ src/pages/ShoppingCarts.tsx
@@ -1,9 +1,13 @@
 import Header from "../components/Navigation/Header";
+import Footer from "../components/Navigation/Footer";
+import ShoppingCart from "../components/Resources/Cart/ShoppingCart";
 
 const ShoppingCarts = () => {
   return (
     <>
       <Header />
+      <ShoppingCart />
+      <Footer />
     </>
   );
 };
