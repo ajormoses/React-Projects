f5e693f7e1536dc00da40d26251d0657dc58bc45
ajormoses <ajormoses@gmail.com>
2025-04-02 19:06:33 +0100

    Add Checkout page to the application

---
+++ src/pages/Checkout.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Navigation/Header";
+import Footer from "../components/Navigation/Footer";
+import CheckoutSteps from "../components/Resources/Checkout/CheckoutSteps";
+
+const Checkout = () => {
+  return (
+    <>
+      <Header />
+      <CheckoutSteps />
+      <Footer />
+    </>
+  );
+};
+
+export default Checkout;
