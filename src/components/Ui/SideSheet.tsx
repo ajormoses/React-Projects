c6b189470d02bd6fb9419a50149acbb4ccded0a6
ajormoses <ajormoses@gmail.com>
2025-04-13 18:18:41 +0100

    Refactor SideSheet component to add custom header and improve styling

---
+++ src/components/Ui/SideSheet.tsx
@@ -0,0 +1,66 @@
+import clsx from "clsx";
+import React, { ReactNode } from "react";
+import { FiChevronLeft } from "react-icons/fi";
+
+interface Props {
+  size?: "sm" | "md" | "lg" | "full";
+  position?: "top" | "bottom";
+  modalWidth?: string;
+  visible: boolean;
+  children?: ReactNode;
+  customHeader?: string;
+  title: string;
+  onClose: () => void;
+}
+
+const UiSideSheet: React.FC<Props> = ({
+  size,
+  position,
+  modalWidth,
+  visible,
+  children,
+  customHeader,
+  title,
+  onClose,
+}) => {
+  return (
+    <div
+      className={clsx(
+        "fixed inset-0 z-30 bg-black bg-opacity-50 transition-opacity duration-300",
+        {
+          "opacity-100 pointer-events-auto": visible,
+          "opacity-0 pointer-events-none": !visible,
+        }
+      )}
+    >
+      <div
+        className={clsx(
+          "flex flex-col gap-6 bg-white h-full transform transition-transform duration-300 ease-in-out ml-auto section",
+          {
+            "translate-x-0": visible,
+            "translate-x-full": !visible,
+
+            "w-full sm:w-1/4": size === "sm",
+            "w-full md:w-1/2": size === "md",
+            "w-full lg:w-3/4": size === "lg",
+            "w-full": size === "full",
+
+            "!h-auto w-full !mx-auto":
+              position === "top" || position === "bottom",
+          },
+          modalWidth
+        )}
+      >
+        <div className={`${customHeader}`}>
+          <div className="flex gap-3 items-center ">
+            <FiChevronLeft className="text-2xl" onClick={onClose} />
+            <p className="text-2xl font-medium">{title}</p>
+          </div>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default UiSideSheet;
