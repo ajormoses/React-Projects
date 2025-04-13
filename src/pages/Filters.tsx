bfb45f356f367a6957eeca2e87089e372c3cd7c1
ajormoses <ajormoses@gmail.com>
2025-04-13 16:30:13 +0100

    Add Filters page to the application

---
+++ src/pages/Filters.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Navigation/Header";
+import HomeFilters from "../components/Resources/Filters";
+import Footer from "../components/Navigation/Footer";
+
+const Filters = () => {
+  return (
+    <>
+      <Header />
+      <HomeFilters />
+      <Footer />
+    </>
+  );
+};
+
+export default Filters;
