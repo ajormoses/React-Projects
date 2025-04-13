2fbcccfaafd29fc5db6a8106f1f014c0a09becd9
ajormoses <ajormoses@gmail.com>
2025-04-13 16:30:24 +0100

    Refactor Dropdown component to use PrimeReact library

---
+++ src/components/Ui/Dropdown.tsx
@@ -0,0 +1,81 @@
+import clsx from "clsx";
+import { Dropdown } from "primereact/dropdown";
+import { useState } from "react";
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+interface Props {
+  options: Option[];
+  customField?: string;
+  placeholder?: string;
+  onChange?: (value: Option | null) => void;
+  showClear?: boolean;
+  error?: string;
+}
+
+const UiDropdown: React.FC<Props> = ({
+  options,
+  customField,
+  placeholder = "Select an option",
+  onChange,
+  showClear,
+  error,
+}) => {
+  const [selected, setSelected] = useState<Option | null>(null);
+
+  const handleChange = (e: any) => {
+    setSelected(e.value);
+    if (onChange) onChange(e.value);
+  };
+
+  return (
+    <>
+      <Dropdown
+        value={selected}
+        onChange={handleChange}
+        options={options}
+        optionLabel="label"
+        showClear={showClear}
+        placeholder={placeholder}
+        auto-filter-focus
+        className={clsx("custom-dropdown", [
+          {
+            "p-invalid !border !border-red-500": error,
+          },
+          customField,
+        ])}
+        pt={{
+          root: {
+            className:
+              "flex justify-between item-center border border-[#D4D4D4] text-primary rounded-md h-[42px] pt-2 px-3",
+          },
+          clearIcon: {
+            class: "absolute right-[45px] mt-1.5",
+          },
+          trigger: {
+            className: "text-primary mt-1.5",
+          },
+          input: {
+            className: "text-primary text-sm",
+          },
+          panel: {
+            className:
+              "bg-white border border-[#D4D4D4] rounded-md shadow-lg mt-1",
+          },
+          item: {
+            className:
+              "px-3 py-2 text-sm text-primary cursor-pointer hover:!bg-[#F5F5F5] outline-none hover:border-transparent",
+          },
+        }}
+      />
+      <small v-if="error" className="p-error">
+        {error}
+      </small>
+    </>
+  );
+};
+
+export default UiDropdown;
