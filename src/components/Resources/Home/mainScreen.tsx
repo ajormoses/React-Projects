5ca5f664702cd51a25dcb80c20f83a1dd988f543
ajormoses <ajormoses@gmail.com>
2025-04-16 23:53:48 +0100

    Refactor main screen component to update UI layout and add new iPhone image

--- src/components/Resources/Home/mainScreen.tsx
+++ src/components/Resources/Home/mainScreen.tsx
@@ -1,30 +1,40 @@
 import Header from "../../Navigation/Header";
 import Btn from "../../Ui/Btn";
 import homeIphone from "../../../assets/img/home-iphone.svg";
+import homeIphone2 from "../../../assets/img/home-iphone-desktop.svg";
 
 const mainScreen = () => {
   return (
     <>
-      <div className="h-screen relative px-4 pt-8 md:flex md:tems-center md:justify-center bg-primary">
+      <div className="bg-primary">
         <Header />
-        <img
-          className="absolute bottom-0 left-1/2 transform -translate-x-1/2 max-w-[343px] max-h-[289px]"
-          src={homeIphone}
-          alt="Home Iphone"
-        />
+        <div className="h-screen md:h-[720px] lg:h-[632px] xl:h-[720px] relative flex lg:flex-row-reverse items-center justify-center lg:justify-start container">
+          <img
+            className="absolute lg:hidden bottom-0 left-1/2 transform -translate-x-1/2 max-w-[343px] max-h-[289px]"
+            src={homeIphone}
+            alt="Home Iphone"
+          />
 
-        <div className="flex gap-2.5 flex-col items-center justify-center mt-[90px] md:mt-0 relative z-10">
-          <p className="font-semibold leading-8 text-lg text-[#909090]">
-            Pro.Beyond.
-          </p>
-          <p className="text-center text-white text-6xl">
-            <span className="font-extralight ">IPhone 14</span> <br />
-            <span className="font-semibold">Pro</span>
-          </p>
-          <p className="font-medium text-lg leading-6 text-[#909090] text-center">
-            Created to change everything for the better. For everyone
-          </p>
-          <Btn customClass="mt-4" label="Shop Now" />
+          <img
+            className="lg:flex hidden max-w-[343px] max-h-[632px] xl:max-w-[406px] xl:max-h-[600px] absolute bottom-0 transform "
+            src={homeIphone2}
+            alt="Home Iphone"
+          />
+
+          <div className="flex gap-2.5 xl:gap-3.5 flex-col items-center justify-center md:items-center lg:items-start lg:mr-auto lg:pt-12  relative z-10">
+            <p className="font-semibold leading-8 text-lg xl:text-[25px] text-[#909090]">
+              Pro.Beyond.
+            </p>
+            <p className="text-center text-white text-6xl xl:text-8xl">
+              <span className="font-thin ">IPhone 14</span>{" "}
+              <br className="md:hidden" />
+              <span className="font-semibold">Pro</span>
+            </p>
+            <p className="font-medium text-lg leading-6 text-[#909090] text-center">
+              Created to change everything for the better. For everyone
+            </p>
+            <Btn customClass="mt-4" label="Shop Now" />
+          </div>
         </div>
       </div>
     </>
