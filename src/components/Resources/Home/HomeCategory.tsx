a5a7f438bcc416e3451155d47626343575931100
ajormoses <ajormoses@gmail.com>
2025-04-24 00:28:49 +0100

    Refactor HomeCategory component to update UI layout and add responsive grid for categories

--- src/components/Resources/Home/HomeCategory.tsx
+++ src/components/Resources/Home/HomeCategory.tsx
@@ -39,28 +39,30 @@ const HomeCategory = () => {
   return (
     <>
       <div className="py-16 px-4 bg-[#FAFAFA]">
-        <div className="flex justify-between items-center gap-4">
-          <p className="font-medium text-2xl">Browse By Category</p>
-          <div className="flex items-center gap-2">
-            <GoChevronLeft className="text-[32px]" />
-            <GoChevronRight className="text-[32px]" />
+        <div className="container">
+          <div className="flex justify-between items-center gap-4">
+            <p className="font-medium text-2xl">Browse By Category</p>
+            <div className="flex items-center gap-2">
+              <GoChevronLeft className="text-[32px]" />
+              <GoChevronRight className="text-[32px]" />
+            </div>
           </div>
-        </div>
-        <div className="mt-10">
-          <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
-            {categories.map((category, index) => (
-              <div
-                key={index}
-                className="flex flex-col items-center justify-center gap-2 p-4 bg-lightGray rounded-[15px]"
-              >
-                <img
-                  src={category.img}
-                  alt={category.gadget}
-                  className="w-16"
-                />
-                <p className="text-sm font-medium">{category.gadget}</p>
-              </div>
-            ))}
+          <div className="mt-10">
+            <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+              {categories.map((category, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col items-center justify-center gap-2 p-4 bg-lightGray rounded-[15px]"
+                >
+                  <img
+                    src={category.img}
+                    alt={category.gadget}
+                    className="w-16"
+                  />
+                  <p className="text-sm font-medium">{category.gadget}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
