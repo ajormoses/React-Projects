5f05005d966fbb1b9470ff8f2ca13a828594cfb1
ajormoses <ajormoses@gmail.com>
2025-04-24 00:28:58 +0100

    Refactor HomeSell component to update grid layout for product display

--- src/components/Resources/Sell/index.tsx
+++ src/components/Resources/Sell/index.tsx
@@ -42,7 +42,7 @@ const HomeSell: React.FC<Props> = ({
   return (
     <div className={clsx(`section ${customSell}`)}>
       {header && <>{header}</>} {/* Slot Content Here */}
-      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 py-6">
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 py-6">
         {products.map((product, index) => (
           <Product
             key={index}
