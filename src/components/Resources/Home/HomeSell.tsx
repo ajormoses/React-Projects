11b832310bc15306c0d4ba1603efce4d5d0bb119
ajormoses <ajormoses@gmail.com>
2025-04-24 00:28:38 +0100

    Refactor HomeSell component to update customSell prop and remove trailing whitespace

--- src/components/Resources/Home/HomeSell.tsx
+++ src/components/Resources/Home/HomeSell.tsx
@@ -70,7 +70,8 @@ const HomeSell = () => {
         btn
         showLike
         truncateTitle
-        customClass="rounded-[9px] py-6 px-3"
+        customSell="md:container"
+        customClass="rounded-[9px] py-6 px-3 "
         customTitle="text-base mb-3"
         customDescription="text-2xl font-medium"
         customBtn="!h-[48px] !py-3 !px-0 !rounded-lg !text-sm !bg-primary !text-white"
