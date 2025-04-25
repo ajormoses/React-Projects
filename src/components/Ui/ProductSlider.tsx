c0bf33d081196a168bd64e5563f798e7a611e955
ajormoses <ajormoses@gmail.com>
2025-04-25 13:41:29 +0100

    Refactor ProductSlider component to update responsive layout and add media query support

--- src/components/Ui/ProductSlider.tsx
+++ src/components/Ui/ProductSlider.tsx
@@ -2,6 +2,8 @@
 import { Pagination, Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Product from "../Resources/Products/ProductInfo";
+import { useMediaQuery } from "../../composables/useMediaQuery";
+import { useCallback } from "react";
 
 // Import Swiper styles
 import "swiper/css";
@@ -22,13 +24,33 @@ const Slider: React.FC<Props> = ({
   customImage,
   customDescription,
 }) => {
+  const isMd = useMediaQuery("(min-width: 768px)");
+  const isLg = useMediaQuery("(min-width: 1024px)");
+  const isXl = useMediaQuery("(min-width: 1200px)");
+
+  const mediaQuerySlide = isXl ? 4 : isLg ? 3 : isMd ? 2 : 1;
+  const mediaQuerySpace = isMd ? 0 : 50;
+
+  const mediaQueryClass = (index: number) => {
+    if (!isMd) return "";
+
+    const pad = "!py-[56px] !px-[32px]";
+    const bgColors = [
+      "!bg-[#FFFFFF]",
+      "!bg-[#F9F9F9]",
+      "!bg-[#EAEAEA]",
+      "!bg-[#2C2C2C]",
+    ];
+
+    return `${bgColors[index] || "!bg-[#FFFFFF]"} ${pad}`;
+  };
   return (
     <>
       <Swiper
         key={Date.now()} // Force update on component render
         modules={[Pagination, Autoplay]}
-        spaceBetween={50}
-        slidesPerView={1}
+        spaceBetween={mediaQuerySpace}
+        slidesPerView={mediaQuerySlide}
         autoplay={{ delay: 2500, disableOnInteraction: false }}
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
@@ -41,9 +63,15 @@ const Slider: React.FC<Props> = ({
               image={slide.image}
               bgColor="transparent"
               btn
-              customBtn="!max-w-[191px]"
+              customBtn={`!max-w-[191px] ${
+                slides.length - 1 === index
+                  ? "md:!bg-[#2C2C2C]"
+                  : "md:!bg-transparent md:!border md:!border-black md:!text-black"
+              }`}
               customImage={customImage}
-              customDescription={customDescription}
+              customDescription={`${customDescription} md:!text-left`}
+              customClass={`${mediaQueryClass(index)} md:!items-start`}
+              customTitle={`${slides.length - 1 === index && "md:!text-white"}`}
             />
           </SwiperSlide>
         ))}
