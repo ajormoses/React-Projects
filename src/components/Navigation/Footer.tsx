0d29967c1614fe51efcfe3d7843a0723c56ddaa1
ajormoses <ajormoses@gmail.com>
2025-04-28 21:24:24 +0100

    Refactor Footer component to update social media icons and responsive layout

--- src/components/Navigation/Footer.tsx
+++ src/components/Navigation/Footer.tsx
@@ -1,4 +1,5 @@
 import Logo from "../../assets/img/white-logo.svg";
+import { FaTwitter, FaFacebookF, FaTiktok, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   const services: {
@@ -28,31 +29,50 @@ const Footer = () => {
       ],
     },
   ];
+
+  const media = [<FaTwitter />, <FaFacebookF />, <FaTiktok />, <FaInstagram />];
+
   return (
     <>
-      <div className="section bg-darkPri flex flex-col gap-4 justify-center items-center">
-        <img src={Logo} alt="logo" className="w-[65.4px] h-[22.87px" />
-        <p className="text-[#CFCFCF] text-[13px] text-center">
-          We are a residential interior design firm located in Portland. Our
-          boutique-studio offers more than
-        </p>
-        {services.map((service, index) => (
-          <div
-            key={index}
-            className={`flex flex-col gap-2 items-center justify-center ${
-              index === 0 ? "mb-8" : ""
-            }`}
-          >
-            <h3 className="text-white font-semibold">{service.title}</h3>
-            <ul className="flex flex-col gap-2 items-center justify-center">
-              {service.list.map((item, index) => (
-                <li key={index} className="text-[#CFCFCF] text-sm">
-                  {item}
-                </li>
-              ))}
-            </ul>
+      <div className="bg-darkPri md:bg-black">
+        <div className="section container">
+          <div className=" flex flex-col gap-4 justify-center items-center md:flex-row md:items-start md:justify-between">
+            <div className="flex flex-col gap-4 items-center md:items-start md:max-w-[384px]">
+              <img src={Logo} alt="logo" className="w-[65.4px] h-[22.87px" />
+              <p className="text-[#CFCFCF] text-[13px] text-center md:text-left">
+                We are a residential interior design firm located in Portland.
+                Our boutique-studio offers more than
+              </p>
+            </div>
+            {services.map((service, index) => (
+              <div
+                key={index}
+                className={`flex flex-col gap-2 items-center justify-center md:items-start ${
+                  index === 0 ? "mb-8" : ""
+                }`}
+              >
+                <h3 className="text-white font-semibold">{service.title}</h3>
+                <ul className="flex flex-col gap-2 items-center justify-center md:items-start">
+                  {service.list.map((item, index) => (
+                    <li key={index} className="text-[#CFCFCF] text-sm">
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
           </div>
-        ))}
+          <div className="hidden md:flex items-center gap-6">
+            {media.map((icon, index) => (
+              <span
+                key={index}
+                className="text-[#CFCFCF] text-base  cursor-pointer hover:text-white transition-all duration-300"
+              >
+                {icon}
+              </span>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
