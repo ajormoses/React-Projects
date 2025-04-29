98cc4817903c8ff0baea83086cf7d7d3bb49523f
ajormoses <ajormoses@gmail.com>
2025-04-29 23:50:20 +0100

    Refactor ProductThumbnails component to add responsive layout and custom class support

--- src/components/Resources/Products/ProductThumbnails.tsx
+++ src/components/Resources/Products/ProductThumbnails.tsx
@@ -4,26 +4,32 @@ import Iphone2 from "../../../assets/img/products/img2.svg";
 import Iphone3 from "../../../assets/img/products/img3.svg";
 import Iphone4 from "../../../assets/img/products/img4.svg";
 
-const ProductDisplay = () => {
+interface Props {
+  customClass?: string;
+}
+
+const ProductDisplay: React.FC<Props> = ({ customClass }) => {
   const [image, setImage] = useState(Iphone);
 
   const thumbnails = [Iphone, Iphone2, Iphone3, Iphone4];
 
   return (
-    <div className="flex flex-col justify-center items-center gap-2 pt-10">
+    <div
+      className={`flex flex-col md:flex-row-reverse justify-center xl:justify-start items-center gap-2 pt-10 md:pt-0 xl:mr-auto ${customClass}`}
+    >
       <img
         src={image}
         alt="Iphone"
-        className="w-[263px] h-[329.24px] mx-auto"
+        className="w-[263px] h-[329.24px] md:h-[516px] md:w-[413.12px] mx-auto md:mx-0 object-cover"
       />
 
-      <div className="grid grid-cols-4 gap-2.5">
+      <div className="grid grid-cols-4 gap-2.5 md:flex md:flex-col md:gap-1">
         {thumbnails.map((item, index) => (
           <img
             key={index}
             src={item}
             alt="Iphone"
-            className={`w-[74.26px] h-[66.34px] cursor-pointer transition-all ${
+            className={`w-[74.26px] h-[66.34px] md:w-[74.88px] md:h-[93px] cursor-pointer transition-all ${
               image === item ? "border-2 border-blue-500 rounded-md" : ""
             }`}
             onClick={() => setImage(item)}
