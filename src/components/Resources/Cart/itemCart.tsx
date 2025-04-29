42c763907ba9b26f43aa547ada50666fcbec7cb9
ajormoses <ajormoses@gmail.com>
2025-04-30 00:32:09 +0100

    Refactor ShoppingCart component to improve layout and add responsive design

--- src/components/Resources/Cart/itemCart.tsx
+++ src/components/Resources/Cart/itemCart.tsx
@@ -39,60 +39,77 @@ const ItemCart: React.FC<Props> = ({ carts: initialCarts }) => {
   };
 
   return (
-    <>
-      {carts.map((cart, index) => (
-        <div
-          className={`flex items-center gap-4 relative pb-12 ${
-            index !== carts.length - 1
-              ? "border-b-[0.5px] border-[#A3A3A3]"
-              : ""
-          }`}
-          key={index}
-        >
-          <motion.img
-            className="h-[90px] w-[90px]"
-            src={cart.item}
-            alt={cart.title}
-            animate={{ rotateY: [0, 15, -10, 0] }}
-            transition={{
-              duration: 1.5,
-              repeat: Infinity,
-              repeatType: "reverse",
-              ease: "easeInOut",
-            }}
-          />
-          <div className="flex flex-col gap-2.5 basis-full">
-            <p className="font-medium max-w-[200px]">{cart.title}</p>
-            <p className="text-sm">{cart.ref}</p>
-            <div className="flex justify-between items-center gap-4">
-              <div className="flex items-center gap-0.5">
-                <Btn
-                  customClass="!w-6 !h-6 !p-4 !bg-transparent !text-black !text-xl"
-                  onClick={() => handleCount(index, count[index] - 1)}
-                  label="-"
-                />
-                <p className="text-black py-2 px-4 border-[0.5px] w-[40px] h-[32px] flex justify-center items-center border-[#D9D9D9] rounded">
-                  {count[index]}
-                </p>
-                <Btn
-                  customClass="!w-6 !h-6 !p-4 !bg-transparent !text-black !text-xl"
-                  onClick={() => handleCount(index, count[index] + 1)}
-                  label="+"
-                />
-                <p className="text-black font-medium text-xl pl-1">
-                  {formatCurrency(cart.price * count[index])}
-                </p>
-              </div>
+    <div className="flex flex-col gap-10">
+      {/* Cart Title */}
+      <h1 className="text-black font-semibold text-2xl leading-6">
+        Shopping Cart
+      </h1>
 
-              <TfiClose
-                onClick={() => RemoveCart(index)}
-                className="text-xl cursor-pointer"
-              />
+      {carts?.length > 0 &&
+        carts.map((cart, index) => (
+          <div
+            className={`flex items-center gap-4 relative pb-12 ${
+              index !== carts.length - 1
+                ? "border-b-[0.5px] border-[#A3A3A3]"
+                : ""
+            }`}
+            key={index}
+          >
+            <motion.img
+              className="h-[90px] w-[90px]"
+              src={cart.item}
+              alt={cart.title}
+              animate={{ rotateY: [0, 15, -10, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "reverse",
+                ease: "easeInOut",
+              }}
+            />
+            <div className="flex flex-col gap-2.5 basis-full">
+              <p className="font-medium max-w-[200px]">{cart.title}</p>
+              <p className="text-sm">{cart.ref}</p>
+              <div className="flex justify-between items-center gap-4">
+                <div className="flex items-center gap-0.5">
+                  <Btn
+                    customClass="!w-6 !h-6 !p-4 !bg-transparent !text-black !text-xl"
+                    onClick={() => handleCount(index, count[index] - 1)}
+                    label="-"
+                  />
+                  <p className="text-black py-2 px-4 border-[0.5px] w-[40px] h-[32px] flex justify-center items-center border-[#D9D9D9] rounded">
+                    {count[index]}
+                  </p>
+                  <Btn
+                    customClass="!w-6 !h-6 !p-4 !bg-transparent !text-black !text-xl"
+                    onClick={() => handleCount(index, count[index] + 1)}
+                    label="+"
+                  />
+                  <p className="text-black font-medium text-xl pl-1">
+                    {formatCurrency(cart.price * count[index])}
+                  </p>
+                </div>
+
+                <TfiClose
+                  onClick={() => RemoveCart(index)}
+                  className="text-xl cursor-pointer"
+                />
+              </div>
             </div>
           </div>
+        ))}
+
+      {carts.length === 0 && (
+        <div className="flex flex-col items-center justify-center py-10 gap-2">
+          <h1 className="text-black font-semibold text-2xl leading-6">
+            Your cart is empty
+          </h1>
+          <p className="text-sm text-[#545454]">
+            Add items to your cart to see them here.
+          </p>
         </div>
-      ))}
-    </>
+      )}
+    </div>
   );
 };
 
