3a13a0152a52eb3f4f19e283833cbcd73b8b06d3
ajormoses <ajormoses@gmail.com>
2025-05-01 19:09:33 +0100

    Refactor tailwind.config.ts to add slide-up animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,9 +26,14 @@ export default {
           "0%": { opacity: 0, transform: "translateY(10px)" },
           "100%": { opacity: 1, transform: "translateY(0)" },
         },
+        "slide-up": {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
       },
       animation: {
         slideFadeIn: "slideFadeIn 0.4s ease-out forwards",
+        "slide-up": "slide-up 0.4s ease-out",
       },
       // backgroundImage: {
       //   primary: "linear-gradient(90.7deg, #211C24 0.64%, #211C24 101%)",
