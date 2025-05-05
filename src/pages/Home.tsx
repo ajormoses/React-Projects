424c6da0f92cc9d389ee9dbb14f82cfdea2aa1c9
ajormoses <ajormoses@gmail.com>
2025-05-05 23:09:27 +0100

    feat: Add Home page with NavigationHeader component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import NavigationHeader from "../components/Navigation/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <NavigationHeader />
+    </div>
+  );
+};
+
+export default Home;
