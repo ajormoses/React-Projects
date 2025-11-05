305bad1818b866ae8e98d5318f9b846b6ba52b30
ajormoses <ajormoses@gmail.com>
2025-11-05 14:15:57 +0100

    feat: Implement scroll-to-top functionality and set up routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
+import { PrimeReactProvider } from "primereact/api";
+import { Routes, Route, useLocation, Navigate } from "react-router";
+import { useEffect } from "react";
 import "./assets/tailwind.css";
 import "@mdi/font/css/materialdesignicons.css";
 
 function App() {
-  return <></>;
+  const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }, [pathname]);
+
+    return null;
+  };
+
+  return (
+    <PrimeReactProvider value={{ unstyled: true }}>
+      <>
+        <ScrollToTop />
+        <Routes>
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/shopping-carts" element={<ShoppingCarts />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/filters" element={<Filters />} /> */}
+
+          {/* Catch-all route */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </>
+    </PrimeReactProvider>
+  );
 }
 
 export default App;
