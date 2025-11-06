dceecec661d0bcf3169c0f8a805e1d12a421a90a
ajormoses <ajormoses@gmail.com>
2025-11-06 09:38:37 +0100

    feat: Enhance Tailwind CSS configuration with custom fonts, colors, keyframes, and animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,52 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Inter"],
+      serif: ["Inter"],
+      body: ["Inter"],
+    },
+    extend: {
+      colors: {
+        border: "#B5B5B5",
+        gray: "#909090",
+        priGray: "#6C6C6C",
+        secGray: "#FAFAFA",
+        lightGray: "#EDEDED",
+        secLightGray: "#F9F9F9",
+        primary: "#211C24",
+        darkPri: "#181313",
+        rating: "#FFB547",
+        darkBlue: "#17183B",
+        darkGray: "#353535",
+      },
+      keyframes: {
+        slideFadeIn: {
+          "0%": { opacity: 0, transform: "translateY(10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+        "slide-up": {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        slowBounce: {
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.02)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
+      animation: {
+        slideFadeIn: "slideFadeIn 0.4s ease-out forwards",
+        "slide-up": "slide-up 0.4s ease-out",
+        "slow-bounce": "slowBounce 4s infinite", // Slow bounce animation
+      },
+      // backgroundImage: {
+      //   primary: "linear-gradient(90.7deg, #211C24 0.64%, #211C24 101%)",
+      // },
+      screens: {
+        "1xl": "1440px",
+      },
+    },
   },
   plugins: [],
 };
