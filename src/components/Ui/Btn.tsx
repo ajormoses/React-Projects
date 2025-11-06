2e4dbee930168a8624081560f763bb1edc8ad20a
ajormoses <ajormoses@gmail.com>
2025-11-06 09:37:00 +0100

    feat: Add Btn and InputField components for reusable UI elements

---
+++ src/components/Ui/Btn.tsx
@@ -0,0 +1,43 @@
+import React, { ReactNode } from "react";
+import clsx from "clsx";
+
+interface BtnProps {
+  label: string;
+  customClass?: string;
+  onClick?: () => void;
+  prependIcon?: ReactNode;
+  appendIcon?: ReactNode;
+  type?: "button" | "submit" | "reset";
+}
+
+const Btn: React.FC<BtnProps> = ({
+  label,
+  customClass,
+  onClick,
+  prependIcon,
+  appendIcon,
+  type,
+}) => {
+  return (
+    <>
+      <button
+        onClick={onClick}
+        className={clsx(
+          `!border flex items-center justify-center border-white py-4 px-14 bg-primary text-white font-medium text-base rounded-md ease-in-out duration-300 hover:scale-95 hover:opacity-90 `,
+          (prependIcon || appendIcon) && "gap-4",
+          customClass
+        )}
+        type={type || "button"}
+      >
+        {/* Prepend Icon */}
+        {prependIcon && prependIcon}
+        {/* label */}
+        {label}
+        {/* Append Icon */}
+        {appendIcon && appendIcon}
+      </button>
+    </>
+  );
+};
+
+export default Btn;
