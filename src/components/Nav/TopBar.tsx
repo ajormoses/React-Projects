cebb518cd21ef63f3d3ad4438f080f3f1965c39a
ajormoses <ajormoses@gmail.com>
2025-11-26 15:24:31 +0100

    feat: topbar UI component responsiveness updated

--- src/components/Nav/TopBar.tsx
+++ src/components/Nav/TopBar.tsx
@@ -12,28 +12,36 @@ interface TabProps {
 const TopBar: React.FC<{
   tabs: TabProps[];
   switchTab: (label: string) => void;
-}> = ({ tabs, switchTab }) => {
+  openDialog: () => void;
+}> = ({ tabs, switchTab, openDialog }) => {
   const navigate = useNavigate();
   const mediaMd = useMediaQuery("(min-width: 768px)");
   return (
     <>
-      <div className="bg-white p-4 rounded-xl flex justify-between items-center">
-        <img
-          src="/img/devlinks.svg"
-          alt="Logo"
-          className="h-8 hidden md:block"
-        />
-        <img src="/img/logo.svg" alt="Logo" className="h-[46px]  md:hidden " />
-
-        {mediaMd && <SwitchTabs tabs={tabs} switchTab={switchTab} />}
-
-        <div className="flex gap-2 items-center">
-          <Btn
-            onClick={() => navigate("/preview")}
-            customClass="!bg-white !border-primary !text-primary !font-semibold hover:!bg-purpleHover !gap-2"
-            label="Preview"
+      <div className="fixed top-0 left-0 right-0 w-full z-50 bg-lightGray">
+        <div className="bg-white !p-4 rounded-xl flex justify-between items-center mt-5 m-auto w-[90%] 2xl:w-[1500px]">
+          <img
+            src="/img/devlinks.svg"
+            alt="Logo"
+            className="h-8 hidden md:block"
           />
-          <UserDropdown />
+          <img
+            onClick={openDialog}
+            src="/img/logo.svg"
+            alt="Logo"
+            className="h-[46px]  md:hidden"
+          />
+
+          {mediaMd && <SwitchTabs tabs={tabs} switchTab={switchTab} />}
+
+          <div className="flex gap-2 items-center">
+            <Btn
+              onClick={() => navigate("/preview")}
+              customClass="!bg-white !border-primary !text-primary !font-semibold hover:!bg-purpleHover !gap-2"
+              label="Preview"
+            />
+            <UserDropdown />
+          </div>
         </div>
       </div>
     </>
